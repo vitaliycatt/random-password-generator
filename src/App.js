7bfef815b09ba3792cee4e74cb9b2457c19a065f
ksowy <90048750+ksowy@users.noreply.github.com>
2022-10-06 16:19:08 +0300

    feat: add styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import styles from './app.module.css'
 
 export const App = () => {
-  return <div className={styles.container}>Main page!</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h1 className={styles.title}>
+          Generate a{' '}
+          <span className={styles.highlightedTitle}>random password</span>
+        </h1>
+        <p className={styles.description}>
+          Never use an insecure password again.
+        </p>
+        <button className={styles.button}>Generate passwords</button>
+        <hr className={styles.line} />
+        <div className={styles.entryField}>
+          <div className={styles.passwordField}>
+            <input type='text' className={styles.password} />
+          </div>
+          <div className={styles.passwordField}>
+            <input type='text' className={styles.password} />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
