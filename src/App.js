f58ce45ff35fd437c0f3b176e0935de4fe3a02e6
vitaliycatt <kso0wek@mail.ru>
2022-10-13 14:39:43 +0300

    feat: add possibility to change theme

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
-import styles from './app.module.css'
+import { useState, useEffect } from "react";
+import styles from "./app.module.css";
+import { GeneratePassword } from "./components/generate-password";
+import { Password } from "./components/password";
 
 export const App = () => {
+  const [password, setPassword] = useState("");
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    if (theme === "dark") {
+      setTheme("light");
+    } else setTheme("dark");
+  };
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${styles[theme]}`}>
       <div className={styles.wrapper}>
+        <span
+          className={
+            theme === "light"
+              ? `${styles.theme} ${styles.sun}`
+              : `${styles.theme} ${styles.moon}`
+          }
+          onClick={toggleTheme}
+        >
+          Switch theme!
+        </span>
         <h1 className={styles.title}>
-          Generate a{' '}
+          Generate a{" "}
           <span className={styles.highlightedTitle}>random password</span>
         </h1>
-        <p className={styles.description}>
+
+        <p
+          className={
+            theme === "light"
+              ? `${styles.description} ${styles.paragraphLight}`
+              : styles.description
+          }
+        >
           Never use an insecure password again.
         </p>
-        <button className={styles.button}>Generate passwords</button>
+
+        <GeneratePassword password={password} setPassword={setPassword} />
+
         <hr className={styles.line} />
-        <div className={styles.entryField}>
-          <div className={styles.passwordField}>
-            <input type='text' className={styles.password} />
-          </div>
-          <div className={styles.passwordField}>
-            <input type='text' className={styles.password} />
-          </div>
+
+        <div className={styles.passwordArea}>
+          <Password password={password} />
+          <Password password={password} />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
